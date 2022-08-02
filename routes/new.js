9806f17913fb48d2afaf9ac36f0c9297578598bb
kisnj <knandor407@gmail.com>
2022-08-02 16:10:44 +0200

    bug fix

--- routes/new.js
+++ routes/new.js
@@ -1,15 +1,22 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const message=require("../message");
-router.get('/', function(req, res, next) {
-  res.render("new",{title:"Add new user"});
+const message = require("../message");
+router.get("/", function (req, res, next) {
+  res.render("new", { title: "Add new user" });
 });
-router.post("/",(req,res)=>{
-  message.create({user:req.body.name,text:req.body.text,added:new Date()}).then(()=>{
-    res.redirect("/")
-  }).catch(e=>{
-    console.log(e)
-  })
-})
+router.post("/", (req, res) => {
+  if (req.body.name !== "" && req.body.text !== "") {
+    message
+      .create({ user: req.body.name, text: req.body.text, added: new Date() })
+      .then(() => {
+        res.redirect("/");
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }else{
+    res.redirect("/");
+  }
+});
 
 module.exports = router;

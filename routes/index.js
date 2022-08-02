3bf392d69759bb139864db995bc40e7e990053b2
kisnj <knandor407@gmail.com>
2022-08-02 15:19:37 +0200

    created core functionality

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require("express");
 var router = express.Router();
-let message=require("../messeges")
+let messages=require("../messeges")
+
+function goTo(res){
+  res.redirect("/new")
+}
 
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Message Board",messages:messages });
+  res.render("index", { title: "Message Board",messages:messages,gotoNew:goTo,res:res });
 });
 
 module.exports = router;
--- routes/new.js
+++ routes/new.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+let messages=require("../messeges")
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render("new",{title:"Add new user"});
 });
 router.post("/",(req,res)=>{
   console.log(req.body)
+  messages.push({user:req.body.name,text:req.body.text,added:new Date()})
   res.redirect("/")
 })
 
